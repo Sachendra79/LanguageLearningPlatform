b66c34a5e02ac3786a1df2d3fbec52c015730bfb
Sachendra79 <sachendragangwar120@gmail.com>
2023-11-06 22:51:08 +0530

    final-setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes,Route } from 'react-router';
+import Login from './pages/Login';
+import Signup from './pages/Signup'
+import Otp from './pages/Otp';
+import Resend from './pages/Resend';
 
 function App() {
   return (
     <div className="App">
      
+   <Routes>
+            <Route  path="/" element={<Login/>} />
+            <Route  path='/signup' element={<Signup/>}  />
+            <Route  path='/otp' element={<Otp/>}  />
+            <Route  path='/resend' element={<Resend/>}  />
+
+   </Routes>
+
+
     </div>
   );
 }
