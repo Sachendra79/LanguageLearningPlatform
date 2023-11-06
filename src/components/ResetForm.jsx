e54e86687f3eb74562c3e647a9e0cf100f06b8dc
Sachendra79 <sachendragangwar120@gmail.com>
2023-11-07 00:16:03 +0530

    adding resetform-option

--- src/components/ResetForm.jsx
+++ src/components/ResetForm.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router'
 import { Link } from 'react-router-dom'
 const ResetForm = () => {
 
-  const[formData,setFormData]=useState({name:"" ,password:""})
+  const[formData,setFormData]=useState({cnfpassword:"" ,password:""})
     const navigate=useNavigate();
 
     function changeHandler(event)
@@ -25,7 +25,40 @@ const ResetForm = () => {
     }
   return (
     <div>
+       <form onSubmit={submitHandler}>
+    
+    <div className="container">
+    
+    <div className="input-group">
       
+      <input
+        type="password"
+        value={formData.password}
+        name='name'
+        onChange={changeHandler}
+        placeholder="Enter username"
+      />
+    </div>
+    <div className="input-group">
+      <label>Password</label>
+      <input
+        type="password"
+        value={formData.password}
+        name='password'
+        onChange={changeHandler}
+        placeholder="Enter password"
+      />
+    </div>
+    <div>
+      <Link to='/reset'>Forgot Password?</Link>
+    </div>
+    <button >Sign in</button> 
+  </div>
+ 
+      
+  </form>
+
+
     </div>
   )
 }
