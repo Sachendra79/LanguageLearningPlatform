1d2167a810205082a4566450ab185aff4547c807
Sachendra79 <sachendragangwar120@gmail.com>
2023-11-08 16:42:28 +0530

    final-form

--- src/components/OtpForm.jsx
+++ src/components/OtpForm.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import { useState ,useRef } from 'react';
 import toast from 'react-hot-toast';
+import OTPInput from 'react-otp-input';
 
 const OtpForm = () => {
   const [otp, setOTP] = useState(['', '', '', '']);
   const inputRefs = [useRef(null), useRef(null), useRef(null), useRef(null)];
 
-  const handleInputChange = (e, index) => {
-    const value = e.target.value;
+  // const handleInputChange = (e, index) => {
+  //   const value = e.target.value;
 
-    if (/^[1-9]$/.test(value) || value === '') {
-      const newOTP = [...otp];
-      newOTP[index] = value;
-      setOTP(newOTP);
+  //   if (/^[1-9]$/.test(value) || value === '') {
+  //     const newOTP = [...otp];
+  //     newOTP[index] = value;
+  //     setOTP(newOTP);
 
-      if (value !== '' && index < 3) {
-        inputRefs[index + 1].current.focus();
-      }
-    }
-  };
+  //     if (value !== '' && index < 3) {
+  //       inputRefs[index + 1].current.focus();
+  //     }
+  //   }
+  // };
   function submitHandler(event)
     {
   event.preventDefault();
@@ -31,34 +32,20 @@ const OtpForm = () => {
       <div className='otp-container'>
       <h1>OTP Login</h1>
       <form onSubmit={submitHandler}>
-        <input
-          type="text"
-          maxLength={1}
-          value={otp[0]}
-          onChange={(e) => handleInputChange(e, 0)}
-          ref={inputRefs[0]}
-        />
-        <input
-          type="text"
-          maxLength={1}
-          value={otp[1]}
-          onChange={(e) => handleInputChange(e, 1)}
-          ref={inputRefs[1]}
-        />
-        <input
-          type="text"
-          maxLength={1}
-          value={otp[2]}
-          onChange={(e) => handleInputChange(e, 2)}
-          ref={inputRefs[2]}
-        />
-        <input
-          type="text"
-          maxLength={1}
-          value={otp[3]}
-          onChange={(e) => handleInputChange(e, 3)}
-          ref={inputRefs[3]}
-        />
+        
+       <div className="middle">
+        <div className="wrapper">
+            <OTPInput
+              value={otp}
+              onChange={setOTP}
+              numInputs={4}
+              renderSeparator={<span></span>}
+              renderInput={(props)=><input {...props}/>}
+              />
+        </div>
+        <div><p className="otp-text">Didn't receive code ?</p></div>
+        <div><a href="#">Resend Code</a></div>
+      </div>
 
         <div><button>Start Learning</button></div>
       </form>
