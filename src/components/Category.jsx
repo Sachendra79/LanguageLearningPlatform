e553a6cc5974b2aec704f5aac698e81c434e49d9
Sachendra79 <sachendragangwar120@gmail.com>
2023-11-27 17:35:00 +0530

    courses-section completed

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,93 +1,33 @@
 
-// import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
-// import { Link, useParams } from 'react-router-dom';
-
-// import { createBrowserHistory } from 'history';
-
-
-// const Category = () => {
-//   const { sector_uuid } = useParams();
-//   const [sectors, setSectors] = useState([]);
-
-//   const history = createBrowserHistory();
-
-  
-//   useEffect(() => {
-//     const fetchSectorsByCategory = async () => {
-//       try {
-//         const response = await axios.get(`https://courses-eduverse.onrender.com/courses/${sector_uuid}`);
-//         setSectors(response.data.data || []); 
-//         console.log("sachin")
-//       } catch (error) {
-//         console.error('Error fetching courses by category:', error);
-//       }
-//     };
-
-//     fetchSectorsByCategory();
-//   }, [sector_uuid]);
-
-//   const handleCategoryClick = (selectedSectorUuid) => {
-//     history.push(`/courses/${selectedSectorUuid}`);
-//   };
-
-
-//   return (
-//     <div className="category-page">
-//       <h2>Category Page</h2>
-     
-// {sectors.map((sector) => (
-//   <div key={sector.sector_uuid} className="sector">
-//     <h3>{sector.sector_name}</h3>
-
-//     <div className="featured-courses">
-//       {Array.isArray(sector.featured_course) && sector.featured_course.map((course) => (
-//         <div key={course.course_uuid} className="course">
-//           <h4>{course.title}</h4>
-//           <p>Author: {course.author.name}</p>
-//           <p>Price: ${course.price}</p>
-
-//           {/* <Link key={course.course_uuid} to={`/courses/detail/${course.course_uuid}`}>
-//             View Details
-//           </Link> */}
-//         </div>  
-//       ))}  
-//     </div>
-
-
-    
-//   </div>
-// ))}
-
-//     </div>
-//   );
-// };
-
-// export default Category;
-
-
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import Sidebar from './Sidebar';
+import Header from './Header';
+import ReactLoading from 'react-loading';
+import courseimage from "../assets/course-image.jpg"
 
 const Category = () => {
   const { sector_uuid } = useParams();
   const [sectors, setSectors] = useState([]);
   const history = createBrowserHistory();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchSectorsByCategory = async () => {
+      setLoading(true)
       try {
         const response = await axios.get(`
         https://courses-eduverse.onrender.com/courses/${sector_uuid}`);
-        setSectors(response.data.sector_uuid || []);
+        setSectors(response.data.data || []);
         console.log("sachin");
         console.log(response)
         
       } catch (error) {
         console.error('Error fetching courses by category:', error);
       }
+      setLoading(false)
     };
 
     fetchSectorsByCategory();
@@ -97,29 +37,63 @@ const Category = () => {
   const handleCategoryClick = (selectedSectorUuid) => {
     history.push(`/courses/${selectedSectorUuid}`);
   };
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
+  const navigate =useNavigate();
+
+ const clickHandler =() =>
+ {
+             navigate("/courses")
+ }
 
   return (
-    <div className="category-page" >
-      <h2>Category Page</h2>
 
-             
-      {sectors.map((sector) => (
-        <div key={sector.sector_uuid} className="sector">
-        <h3>{sector.sector_name}</h3>
-      
-        <div className="featured-courses" key={sector.sector_uuid}>
-          {Array.isArray(sector.featured_course) && sector.featured_course.map((course) => (
-            <div key={course.course_uuid} className="course">
-              <h4>{course.title}</h4>
-              <p>Author: {course.author.name}</p>
-              <p>Price: ${course.price}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-      
-      ))}
+<div className='main-container grid-container'>
+      <div className='sidebar'>  <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/></div>
+      <div className='right-side' >
+             <div className='header'> <Header OpenSidebar={OpenSidebar}/></div>
+             <div className='content'>
+           
+    <div className="category-page" >
+                <button onClick={clickHandler} className='mb-5 mt-2 border-2 border-solid border-black px-3 w-36 font-semibold text-[#000] rounded-xl bg-white'>Back
+                </button>
+      <div className='featured-courses gap-5'>
+
+      {loading ? (
+              <ReactLoading type='spin' color="blue" height={60} width={60} className='loader' />
+            ) : ( 
+  
+      sectors.map(course => (
+
+        <div key={course.course_uuid} className='courses'>
+            <img src={courseimage} className='rounded-xl' ></img>
+          <h4 className='text-[#000] font-semibold' >Title: {course.title}</h4>
+          <p className='course-author text-[#000]' >Author: {course.author.name}</p>
+
+          <p className='course-price text-[#b33838]'>Price: {course.price}</p>
+          <Link className='course-detail border-2 border-solid border-slate-800 p-1 bg-red-700 rounded-2xl'
+                          to={`/courses/detail/${course.course_uuid}`}
+                        >
+                          View Details
+                        </Link>
+         </div>
+      )) ) }
+
+       </div>
+
     </div>
+             </div>
+
+      </div>
+    </div>
+
+
+
+
+
   );
           
 
--- src/pages/Course.js
+++ src/pages/Course.js
@@ -135,6 +135,7 @@ import ReactLoading from 'react-loading';
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
 import courseimage from '../assets/course-image.jpg';
+import { useNavigate } from 'react-router-dom';
 
 const Course = () => {
   const [sectors, setSectors] = useState([]);
@@ -142,6 +143,7 @@ const Course = () => {
   const { sector_uuid } = useParams();
   const [loading, setLoading] = useState(false);
   const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
+  const [courses, setCourses] = useState([]);
 
   const OpenSidebar = () => {
     setOpenSidebarToggle(!openSidebarToggle);
@@ -166,13 +168,17 @@ const Course = () => {
   }, [sector_uuid]);
 
   const handleSearch = async () => {
+    setLoading(true);
     try {
+     
       const response = await axios.get(`https://courses-eduverse.onrender.com/courses/search/${searchTerm}`);
-      setSectors(response.data);
-      console.log(response);
+      
+     
+      setCourses(response.data);
     } catch (error) {
-      console.error('Error fetching courses by search term:', error);
+      console.error('Error fetching courses:', error);
     }
+    setLoading(false);
   };
 
   const handleCategoryFilter = async (selectedSectorUuid) => {
@@ -183,6 +189,13 @@ const Course = () => {
       console.error('Error fetching courses by category:', error);
     }
   };
+  const navigate =useNavigate();
+
+  const clickHandler =() =>
+  {
+              navigate("/courses")
+  }
+ 
 
   return (
     <div className='main-container grid-container'>
@@ -196,20 +209,45 @@ const Course = () => {
         <div className='content'>
           <div className="course-page flex flex-col gap-5">
             <div className='flex flex-wrap'>
-              <h1 className='course-heading ml-[2vw] text-[#e42f2f] text-4xl font-bold'>Courses</h1>
+         <h1 className='course-heading ml-[2vw] text-[#e42f2f] text-4xl font-bold'>Courses</h1>
+       
+<div className='m-auto text-3xl'>
+    <input
+      type="text"
+      placeholder="Search by title"
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+      className='rounded-xl boder-solid border-black border-2 text-[#8e2d2d]'
+    />
+    <button onClick={handleSearch} className='search-button bg-[#594ed3] text-[#000] ml-3 rounded-xl text-3xl pt-2'>
+      <p className='search-text'>Search</p>
+    </button>
+    </div>
+    <div className='featured-courses mt-10'>
+   
+{loading ? (
+        <ReactLoading type='spin' color="blue" height={60} width={60} className='loader' />
+      ) : ( 
 
-              {/* Search Bar */}
-              <div className='m-auto text-3xl'>
-                <input
-                  type="text"
-                  placeholder="Search by title "
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                  className='rounded-xl boder-solid border-black border-2'
-                />
-                <button onClick={handleSearch} className='bg-[#594ed3] text-[#000] ml-3 rounded-xl '>Search</button>
-              </div>
-            </div>
+courses.map(course => (
+           
+  <div key={course.id} className='courses'>
+        
+      <img src={courseimage} className='rounded-xl' ></img>
+    <h4 className='text-[#000] font-semibold' > {course.title}</h4>
+    <p className='course-author text-[#000]' > {course.author.name}</p>
+
+    <p className='course-price text-[#b33838]'>Price: {course.price}</p>
+    <Link className='course-detail border-2 border-solid border-slate-800 p-1 bg-red-700 rounded-2xl'
+                    to={`/courses/detail/${course.course_uuid}`}
+                  >
+                    View Details
+                  </Link>
+   </div>
+)) ) }
+
+ </div>
+      </div>
 
             <Link to="/courses" className='text-xl text-[#000] font-semibold'>Category Present</Link>
 
@@ -226,22 +264,6 @@ const Course = () => {
     </Link>
   ))}
 </div>
-{/* 
-<div className='category-filters flex flex-wrap gap-5 text-[#000] font-semibold '>
-        <Link to="/courses" onClick={() => setSectors([])} className='bg-[#fff] border-black border-solid border-2 px-2 rounded-xl'>
-          All
-        </Link>
-        {sectors.map((sector) => (
-          <Link
-            key={sector.sector_uuid}
-            to={`/courses/${sector.sector_uuid}`}
-            onClick={() => handleCategoryFilter(sector.sector_uuid)}
-            className='border-black border-solid border-2 px-3 bg-[#fff] rounded-xl'
-          >
-            {sector.sector_name}
-          </Link>
-        ))}
-      </div> */}
 
             {loading ? (
               <ReactLoading type='spin' color="blue" height={60} width={60} className='loader' />
